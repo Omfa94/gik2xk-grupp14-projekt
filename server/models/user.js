de87a529c4fef36cd3820dca957d5173383690d8
Omfa94 <147593282+Omfa94@users.noreply.github.com>
2024-03-10 23:08:44 +0100

    lek2p1

---
+++ server/models/user.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define("user", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firstName: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(200),
+      allowNull: false,
+      validate: {
+        len: [4, 200],
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+      validate: {
+        len: [10, 20],
+      },
+    },
+    // {
+    //   underscored: true
+    // }
+  });
+};
