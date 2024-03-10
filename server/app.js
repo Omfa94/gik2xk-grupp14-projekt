8adbdf1e89ba86ecb3c0be98fbea5b167df3eac7
Omfa94 <147593282+Omfa94@users.noreply.github.com>
2024-03-10 21:06:29 +0100

    package installation

--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 var app = express();
 
 app.use(logger('dev'));
@@ -10,5 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 module.exports = app;
