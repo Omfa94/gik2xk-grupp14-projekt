38b44bbcb178aed55f1e3c394fbc6e4f8ee54631
Omfa94 <147593282+Omfa94@users.noreply.github.com>
2024-03-11 17:34:10 +0100

    L2.4 Validate

--- server/routes/productsRoute.js
+++ server/routes/productsRoute.js
@@ -1,5 +1,17 @@
 const router = require("express").Router();
 const db = require("../models");
+const validate = require("validate.js");
+
+const constraints = {
+  title: {
+    length: {
+      minimum: 2,
+      maximum: 100,
+      tooShort: "^Titeln måste vara minst %{count} tecken lång.",
+      tooLong: "^Titeln får inte vara längre än %{count} tecken lång.",
+    },
+  },
+};
 
 router.get("/", (req, res) => {
   db.product.findAll().then((result) => {
@@ -8,23 +20,37 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  db.product.create(req.body).then((result) => {
-    res.send(result);
-  });
+  const product = req.body;
+  const invalidData = validate(product, constraints);
+  if (invalidData) {
+    res.status(400).json(invalidData);
+  } else {
+    db.product.create(product).then((result) => {
+      res.send(result);
+    });
+  }
 });
 
 router.put("/", (req, res) => {
-  db.product
-    .update(req.body, {
-      where: { id: req.body.id },
-    })
-    .then((result) => {
-      res.send(result);
-    });
+    const product = req.body;
+    const invalidData = validate(product, constraints);
+    const id = product.id;
+    if(invalidData || !id ){
+        res.status(400).json(invalidData || "Id är obligatoriskt ");
+    }else{
+        db.product
+        .update(product, {
+          where: { id: product.id },
+        })
+        .then((result) => {
+          res.send(result);
+        });
+    }
+
 });
 
 router.delete("/", (req, res) => {
-    db.product
+  db.product
     .destroy({
       where: { id: req.body.id },
     })
