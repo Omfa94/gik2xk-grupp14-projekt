00b71209cd9bf80f8ac276300cb2fc244983e5e7
Omfa94 <147593282+Omfa94@users.noreply.github.com>
2024-03-16 14:54:53 +0100

    L4.4

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import ProductEdit from "./views/ProductEdit.jsx";
+import Home from './views/Home.jsx';
+import Products from './views/Products.jsx';
+import ProductDetail from './views/ProductDetail.jsx';
+import Cart from "./views/Cart.jsx"
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+
+      { path: "/products/:id/edit", element: <ProductEdit /> },
+      { path: "/products/new", element: <ProductEdit /> },
+      { path: "/products/", element: <Products /> },
+      { path: "/products/:id", element: <ProductDetail /> },
+      { path: "/cart/new", element: <Cart /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
