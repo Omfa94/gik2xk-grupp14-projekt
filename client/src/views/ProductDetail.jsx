f322e55eb695904a10c047421596a980b4e5967d
Omfa94 <147593282+Omfa94@users.noreply.github.com>
2024-03-18 20:52:30 +0100

    L6.4

--- client/src/views/ProductDetail.jsx
+++ client/src/views/ProductDetail.jsx
@@ -2,36 +2,47 @@ import ProductItemLarge from "../components/ProductItemLarge";
 import RatingForm from "../components/RatingForm";
 import CustomRating from "../components/Rating";
 import {Button,} from "@mui/material";
-import {useNavigate} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getOne } from "../sevices/ProductService";
 
 function ProductDetail() {
-    const product= {
-        "id": 3,
-        "title": "En fin Volvo som är BMW",
-        "description": "Om du kör den så kommer du tycka att du flyger.",
-        "price": 1000,
-        "imageUrl": "http://dimo.com",
-        "createdAt": "2024-03-16T22:05:38.000Z",
-        "updatedAt": "2024-03-16T22:05:38.000Z",
-        "ratings": [
-            {
-                "rating": 5,
-                "author": "Dimo Faroughi",
-                "createdAt": "2024-03-16T22:07:36.000Z"
-            }
-        ]
-    };
+    // const product= {
+    //     "id": 3,
+    //     "title": "En fin Volvo som är BMW",
+    //     "description": "Om du kör den så kommer du tycka att du flyger.",
+    //     "price": 1000,
+    //     "imageUrl": "http://dimo.com",
+    //     "createdAt": "2024-03-16T22:05:38.000Z",
+    //     "updatedAt": "2024-03-16T22:05:38.000Z",
+    //     "ratings": [
+    //         {
+    //             "rating": 5,
+    //             "author": "Dimo Faroughi",
+    //             "createdAt": "2024-03-16T22:07:36.000Z"
+    //         }
+    //     ]
+    // };
+   
+    const {id}= useParams();
+
+    const [product,setproduct]=useState(null);
+    
+    useEffect(()=>{
+        getOne(id).then((product)=>setproduct(product));
+    },[id]);
+
 
     const navigate=useNavigate();
 
-    return (<div>
+    return product? (<div>
     <ProductItemLarge product={product}/>
     <Button onClick={()=>navigate(-1)}>Tillbaka</Button>
     <Button onClick={()=>navigate(`/products/${product.id}/edit`)}>Ändra</Button>
     <Button>Lägg till varokorg</Button>
     <RatingForm/>
     {product.ratings && product.ratings.map((rating,i)=> <CustomRating key={`rating_${i}`} rating={rating}/>)}
-    </div>);
+    </div>):(<h3>Kunde inte hämta produkt</h3>);
 }
 
 export default ProductDetail;
--- client/src/views/ProductEdit.jsx
+++ client/src/views/ProductEdit.jsx
@@ -1,4 +1,23 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getOne } from "../sevices/ProductService";
+
 function ProductEdit() {
+    const {id}= useParams();
+
+    const [product,setproduct]=useState(null);
+    
+    useEffect(()=>{
+        if(id){
+            getOne(id).then((product)=>setproduct(product));
+        }else{
+            setproduct(null);
+        }
+        
+    },[id]);
+
+    console.log(product)
+
     return ( <h2>ProductEdit</h2> );
 }
 
