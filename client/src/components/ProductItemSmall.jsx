5a7e78492570f4e80d420d98b358e15a21e31f2c
Omfa94 <147593282+Omfa94@users.noreply.github.com>
2024-03-19 22:10:51 +0100

    L7.3

--- client/src/components/ProductItemSmall.jsx
+++ client/src/components/ProductItemSmall.jsx
@@ -1,22 +1,61 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+import placeholderImage from "../assets/placeholder.png";
+import {truncate } from "../common/FormatHelper"
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
-
-function ProductItemSmall({product}) {
+function ProductItemSmall({ product }) {
   const formatDate = (dateString) => {
-    const options = { year: 'numeric', month: 'long', day: 'numeric' }; 
-    return new Intl.DateTimeFormat('sv-SE', options).format(new Date(dateString));
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return new Intl.DateTimeFormat("sv-SE", options).format(
+      new Date(dateString)
+    );
   };
 
   return (
     <>
-      <Link to={`/products/${product.id}`}>
-        <h3>{product.title}</h3>
-      </Link>
-      <p>Beskrivning: {product.description}</p>
-      <p>Pris: {product.price} kr</p>
-      <p>Skapad: {formatDate(product.createdAt)}</p> {/* hur du kan använda formatDate-funktionen */}
-      <img src={product.imageUrl} alt={product.title} style={{ width: '200px', height: 'auto' }} /> {/* bildvisning */}
+      <Card variant="outlined" sx={{ mb: 4 }}>
+        <CardHeader
+          title={
+            <Typography variant="h3">
+              <Link to={`/products/${product.id}`}>{product.title}</Link>
+            </Typography>
+          }
+          subheader={`Skapad den: ${formatDate(product.createdAt)}`}
+        />
+
+        <CardMedia
+          component="img"
+          height="200"
+          image={product.imageUrl || placeholderImage}
+          alt={`Bild till ${product.title}`}
+        />
+        <CardContent>
+          <Typography variant="body3">{truncate(product.description,200)}
+          </Typography>
+        </CardContent>
+        <Box mb={2} ml={2}>
+          <Typography variant="h6">Pris: {product.price} SEK</Typography>
+        </Box>
+
+        <CardActions sx={{display: "flex", justifyContent:"end"}}>
+          <Button
+          component={Link} 
+          to={`/products/${product.id}`}
+          endIcon={<ChevronRightIcon/>}>Läs mer</Button>
+        </CardActions>
+
+      </Card>
     </>
   );
 }
