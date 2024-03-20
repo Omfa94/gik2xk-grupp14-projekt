66125db24eab9f41fe1f3ce3034ceb5192bb149a
Kirkerino <williamtrusler@rocketmail.com>
2024-03-20 15:15:44 +0100

    revert productService, productsRoute, userService, usersRoute

--- server/routes/productsRoute.js
+++ server/routes/productsRoute.js
@@ -1,47 +1,73 @@
-const router = require('express').Router();
-const productService = require('../services/productService');
+const router = require("express").Router();
+const productService = require("../services/productService");
 
-router.get('/', (req, res) => {
-    productService.getAll().then(result => {
-        res.status(result.status).json(result.data);
-    });
+
+ 
+
+//adda rating till produkten
+router.post("/:id/addRating", (req, res) => {
+  const rating = req.body;
+  const id = req.params.id;
+  productService.addRating(id, rating).then((result) => {
+    res.status(result.status).json(result.data);
+  });
 });
 
-router.get('/:id', (req, res) => {
-    const id = req.params.id;
-    productService.getById(id).then(result => {
-        res.status(result.status).json(result.data);
-    });
+// async function getById(id) {
+//   try {
+//     const product = await db.product.findOne({
+//       where: { id },
+//       include: [
+//         db.rating,
+//         {
+//           model: db.rating,
+//           include: [db.user],
+//         },
+//       ],
+//     });
+//     return createResponseSuccess(_formatProduct(product));
+//   } catch (error) {
+//     return createResponseError(error.status, error.message);
+//   }
+// }
+
+//Hämta en specifik produkt.
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+
+  productService.getById(id).then((result) => {
+    res.status(result.status).json(result.data);
+  });
 });
 
-router.post('/', (req, res) => {
-    const product = req.body;
-    productService.create(product).then(result => {
-        res.status(result.status).json(result.data);
-    });
+//hämtar alla produkter
+router.get("/", (req, res) => {
+  productService.getAll().then((result) => {
+    res.status(result.status).json(result.data);
+  });
 });
 
-router.post('/:id/addRating', (req, res) => {
-    const id = req.params.id;
-    const rating = req.body;
-    productService.addRating(id, rating).then(result => {
-        res.status(result.status).json(result.data);
-    });
+router.post("/", (req, res) => {
+  const product = req.body;
+  productService.create(product).then((result) => {
+    res.status(result.status).json(result.data);
+  });
 });
 
-router.put('/', (req, res) => {
-    const product = req.body;
-    const id = product.id;
-    productService.update(product, id).then(result => {
-        res.status(result.status).json(result.data);
-    })
+router.put("/", (req, res) => {
+  const product = req.body;
+  const id = product.id;
+
+  productService.update(product, id).then((result) => {
+    res.status(result.status).json(result.data);
+  });
 });
 
-router.delete('/', (req, res) => {
-    const id = product.id;
-    productService.destroy(id).then(result => {
-        res.status(result.status).json(result.data);
-    })
+router.delete("/", (req, res) => {
+  const id = req.body.id;
+  productService.destroy(id).then((result) => {
+    res.status(result.status).json(result.data);
+  });
 });
 
 module.exports = router;
--- server/services/productService.js
+++ server/services/productService.js
@@ -1,157 +1,153 @@
-const db = require('../models');
+const db = require("../models");
 const {
-    createResponseSuccess,
-    createResponseError,
-    createResponseMessage
-} = require('../helpers/responseHelper');
-const validate = require('validate.js');
+  createResponseSuccess,
+  createResponseError,
+  createResponseMessage,
+} = require("../helpers/responseHelper");
+const validate = require("validate.js");
+
+
 const constraints = {
-    title: {
-        length: {
-            minimum: 2,
-            maximum: 100,
-            tooShort: "^Title needs to be at least %{count} characters long",
-            tooLong: "^Title can be a maximum of %{count} characters long",
-        },
-        presence: {
-            allowEmpty: false,
-            message: "^Title can not be empty"
-        }
+  title: {
+    length: {
+      minimum: 2,
+      maximum: 100,
+      tooShort: "^Titeln måste vara minst %{count} tecken lång.",
+      tooLong: "^Titeln får inte vara längre än %{count} tecken lång.",
     },
-    price: {
-        numericality: {
-            greaterThan: 0,
-            message: "^Price must be a number greater than 0"
+  },
+};
+
+//logik att lägga till produkter i varokorgen. flytta från cart hit.
+// async function addProductToCart(){};
+
+async function getById(id) {
+  try {
+    const product = await db.product.findOne({
+      where: { id },
+      include: [
+        db.rating,
+        {
+          model: db.rating,
+          include: [db.user],
         },
-        presence: {
-            allowEmpty: false,
-            message: "^Product must have a price"
-        }
-    },
-    imageUrl: {
-        url: {
-            message: "^URL is not in a valid format",
-            allowLocal: true,
-        }
-    }
+      ],
+    });
+    return createResponseSuccess(_formatProduct(product));
+  } catch (error) {
+    return createResponseError(error.status, error.message);
+  }
 }
 
 async function getAll() {
-    try {
-        const allProducts = await db.product.findAll({
-            include: [db.rating]
-        });
-        return createResponseSuccess(allProducts.map(product => _formatProduct(product)));
-    } catch (error) {
-        return createResponseError(error.status, error.message);
-    }
+  try {
+    const allProducts = await db.product.findAll({
+      include: [db.rating,
+        {
+          model: db.rating,
+          include: [db.user],
+        }]
+    });
+    return createResponseSuccess(allProducts.map(product=>_formatProduct(product)));
+  } catch (error) {
+    return createResponseError(error.status, error.message);
+  }
 }
 
-async function getById(id) {
-    if(!id) {
-        return createResponseError(422, 'ID is required');
-    }
-    try {
-        const product = await db.product.findOne({
-            where:{id},
-            include: [db.rating]
-        });
-        return createResponseSuccess(_formatProduct(product));
-    } catch (error) {
-        return createResponseError(error.status, error.message);
-    }
+async function addRating(id, rating, userId) { // Anta att userId nu är en parameter
+  if (!id) {
+    return createResponseError(422, "Id är obligatoriskt");
+  }
+  try {
+    // Skapa en ny instans av rating-objektet med nödvändiga ändringar
+    const ratingToSave = { ...rating, productId: id, userId: userId };
+    const newRating = await db.rating.create(ratingToSave);
+    return createResponseSuccess(newRating);
+  } catch (error) {
+    // Antag att createResponseError kan hantera okända eller oväntade fel
+    return createResponseError(error.status || 500, error.message || "Ett oväntat fel inträffade");
+  }
 }
+
 
 async function create(product) {
-    const invalidData = validate(product, constraints);
-    if(invalidData) {
-        return createResponseError(422, invalidData);
-    }
-    try {
-        const newProduct = await db.product.create(product);
-        return createResponseSuccess(newProduct);
-    } catch (error) {
-        return createResponseError(error.status, error.message);
-    }
-}
+  const invalidData = validate(product, constraints);
+  if (invalidData) {
+    return createResponseError(422, invalidData);
+  }
+  try {
+    const newProduct = await db.product.create(product);
 
+    return createResponseSuccess(newProduct);
+  } catch (error) {
+    return createResponseError(error.status, error.message);
+  }
+}
 async function update(product, id) {
-    const invalidData = validate(product, constraints);
-    if(!id) {
-        return createResponseError(422, 'ID is required');
-    }
-    if(invalidData || !id) {
-        return createResponseError(422, invalidData);
-    }
-    try {
-        await db.product
-        .update(product, {
-            where: {id}
-        });
-        return createResponseMessage(200, 'Product updated');
-    } catch (error) {
-        return createResponseError(error.status, error.message);
-    }
+  const invalidData = validate(product, constraints);
+  if (!id) {
+    return createResponseError(422, "Id är obligatoriskt");
+  }
+  if (invalidData) {
+    return createResponseError(422, invalidData);
+  }
+  try {
+    await db.product.update(product, {
+      where: { id },
+    });
+    return createResponseMessage(200, "Produkten uppdaterades.");
+  } catch (error) {
+    return createResponseError(error.status, error.message);
+  }
 }
-
 async function destroy(id) {
-    if(!id) {
-        return createResponseError(422, 'ID is required');
-    } 
-    try {
-        await db.product
-        .destroy({
-            where: {id}
-        });
-        return createResponseMessage(200, 'Product deleted');
-    } catch (error) {
-        return createResponseError(error.status, error.message);
-    }
+  if (!id) {
+    return createResponseError(422, "Id är obligatoriskt");
+  }
+  try {
+    await db.product.destroy({
+      where: { id },
+    });
+    return createResponseMessage(200, "Produkten raderades.");
+  } catch (error) {
+    return createResponseError(error.status, error.message);
+  }
 }
 
-async function addRating(id, rating) {
-    if(!id) {
-        return createResponseError(422, 'ID is required');
-    }
-    try {
-        rating.productId = id;
-        const newRating = await db.rating.create(rating);
-        return createResponseSuccess(newRating);
-    } catch (error) {
-        return createResponseError(error.status, error.message);
-    }
-}
 
 function _formatProduct(product) {
     const cleanProduct = {
-        id: product.id,
-        title: product.title,
-        description: product.description,
-        price: product.price,
-        imageUrl: product.imageUrl,
-        createdAt: product.createdAt,
-        updatedAt: product.updatedAt,
-    }
+      id: product.id,
+      title: product.title,
+      description: product.description,
+      price: product.price,
+      imageUrl: product.imageUrl,
+      createdAt: product.createdAt, 
+      updatedAt: product.updatedAt, 
+      ratings: [] 
+    };
+  
+    // Format ratings if they exist
     if (product.ratings) {
-        cleanProduct.ratings = [];
-        product.ratings.map(rating => {
-            return(cleanProduct.ratings = [
-                {
-                    rating: rating.rating,
-                    createdAt: rating.createdAt,
-                    updatedAt: rating.updatedAt
-                }, 
-                ...cleanProduct.ratings
-            ]);
-        });
+      cleanProduct.ratings = product.ratings.map((rating) => {
+        return {
+          rating: rating.rating,
+          author: rating.user ? `${rating.user.firstName} ${rating.user.lastName}` : 'Anonym',
+          createdAt: rating.createdAt 
+        };
+      });
     }
+  
     return cleanProduct;
-}
+  }
+  
 
 module.exports = {
-    getAll,
-    getById,
-    create,
-    update,
-    destroy,
-    addRating}
+  // addProductToCart,
+  getById,
+  getAll,
+  addRating,
+  create,
+  update,
+  destroy,
+};
